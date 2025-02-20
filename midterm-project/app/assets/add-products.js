ec435ca1adf31c86a90c4ca14ffbd14b87ed23f7
Jonas Shoukri <116384674+JonaShoukri@users.noreply.github.com>
2025-02-20 11:14:45 -0500

    ready for pull request

---
+++ midterm-project/app/assets/add-products.js
@@ -0,0 +1,4 @@
+document.getElementById("add-product-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Product added successfully (not functional yet).");
+});
